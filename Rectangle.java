7f6946c983b7cb4d092b1d23b7432e972817e28d
Nilesh Ingale <37446548+nileshingale11@users.noreply.github.com>
2018-12-22 12:37:03 +0530

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -2,9 +2,6 @@ package shape;
 
 public class Rectangle extends Shape {
 	private int length, breadth;
-	public void myShape() {
-		System.out.println("Hi. I'm a Rectangle.");
-	}
 	public Rectangle(int l, int b) {
 		length = l;
 		breadth = b;
@@ -15,6 +12,10 @@ public class Rectangle extends Shape {
 	}
 	public int getArea() {
 		return length * breadth;
+	}
+	public Square convertToSquare(int s) {
+		Square sq = new Square(s);
+		return sq;
 	}
 
 }
