85e52a0597fcc18a9edd1d05383cff39a32851dd
Nilesh Ingale <37446548+nileshingale11@users.noreply.github.com>
2019-01-21 16:02:24 +0530

    Add files via upload
    All datatype typecasting methods in a single file.

---
+++ TypeCast.java
@@ -0,0 +1,19 @@
+public class TypeCast{
+		
+	public static int convertToInt(String str) {
+		return Integer.parseInt(str);
+	}
+	public static double convertToDouble(String str) {
+		return Double.parseDouble(str);
+	}
+	public static float convertToFloat(String str) {
+		return Float.parseFloat(str);
+	}
+	public static long convertToLong(String str) {
+		return Long.parseLong(str);
+	}
+	public static short convertToShort(String str) {
+		return Short.parseShort(str);
+	}
+	
+}
